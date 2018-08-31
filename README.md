587f8f65b1d6a2d94b2e249e51f1e25685dc1b2c
Justin Le <justinhle@hotmail.com>
2018-08-31 01:24:10 -0700

    Editted Function to plot multiple charts and cleaned data better.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # data_visuals
 This is the start of some side projects regarding data visualizations with Python. This is to help me develop my self taught Data Science/Analyst skills to work with data using Python (pandas, numpy, seaborm, matplotlib, etc). Inspired by aaronpenne who helped introduce me to the world of data science.
 
-1 Bedroom Apartment Monthly Rent Plots 
+1 Bedroom Apartment Monthly Rent Plots 2010 - 2018
 ----------------
 Still in the works of mapping onto SVG and adding Median Income Data
-![bar plot](https://github.com/lejh1/data_visuals/blob/master/rent_comparison/visuals/bar1.png)
+![bar plot](https://github.com/lejh1/data_visuals/blob/master/rent_comparison/visuals/bar103.png)
 Data: [Zillow Data](https://www.zillow.com/research/data/)
+![bar plot gif](https://github.com/lejh1/data_visuals/blob/master/rent_comparison/visuals/bar.gif)
+
--- rent_comparison/rent_comparison.py
+++ rent_comparison/rent_comparison.py
@@ -1,7 +1,7 @@
 ''' 
 Author: lejh1 - Justin Le
 Date of Creation: 08/30/2018
-Lst Modified Date: 08/30/2018
+Lst Modified Date: 08/31/2018
 '''
 
 import os 
@@ -25,30 +25,38 @@ if not os.path.isdir(visuals_dir):
 df = pd.read_csv(csv_path)
 
 def BarPlotCA(old_df):
-    df = old_df[['RegionName','State','2018-07']].copy()
-
-    '''columns_to_keep = list(df)
-    del columns_to_keep[2:6]
-    print(columns_to_keep)'''
-
     # Data Cleaning and Manipulation 
+    columns_to_keep = list(old_df)
+    del columns_to_keep[2:6]
+    df = old_df[columns_to_keep].copy()
     df.where(df['State'] == 'CA',inplace = True)
-    df.dropna(inplace = True)
-    df.sort_values(by=['2018-07'], inplace = True)
+    df.dropna(subset = ['State'], inplace = True)
 
-    x = list(df['RegionName'])
-    x_pos = np.arange(len(x))
-    y = list(df['2018-07'])
-    plt.figure(figsize = (16,9)) #sizing
-    plt.bar(x_pos, y, align = 'center', alpha = 1)
-    plt.xticks(x_pos, x, rotation = 45)
-    plt.ylabel('Monthly Median Rent')
-    plt.title('CA 1 Bedroom Median Monthly Rent')
-    # plt.show()
+    # print(df)
+    skip = 2
+    for index, column in enumerate(df):
+        if skip:
+            skip -= 1
+            print(str(index) + ' ' + column)
 
-    plt.savefig(os.path.join(visuals_dir, 'bar1.png'),dpi=150)
+        else:
+            # df.sort_values(by=[column], inplace = True)
 
-# BarPlotCA(df)
+            x = list(df['RegionName'])
+            x_pos = np.arange(len(x))
+            y = list(df[column])
+            plt.figure(figsize = (16,9)) #sizing
+            plt.bar(x_pos, y, align = 'center', alpha = 1)
+            plt.xticks(x_pos, x, rotation = 45)
+            plt.ylabel('Monthly Median Rent')
+            title = 'CA 1 Bedroom Median Monthly Rent - ' + column
+            plt.title(title)
+            # plt.show()
+            s_name = 'bar' + str(index) + '.png'
+            plt.savefig(os.path.join(visuals_dir, s_name),dpi=150)
+            plt.close()
+
+BarPlotCA(df)
 
 # with open(map_path, 'r') as map:
 #     for line in map:

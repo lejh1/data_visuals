98c20588ab43feb00a850097d6fcfe5cf3b42b75
Justin Le <justinhle@hotmail.com>
2018-09-08 01:21:57 -0700

    added parsing logic

--- pokemon/classification_names.py
+++ pokemon/classification_names.py
@@ -10,9 +10,13 @@ import matplotlib as plt
 
 cur_dir = os.getcwd()
 data_dir = os.path.join(cur_dir, "data")
-poke_path = os.path.join(data_dir, "pokemon.csv")
-visuals_path = os.path.join(cur_dir, "visuals")
+poke_dir = os.path.join(data_dir, "pokemon.csv")
+visuals_dir = os.path.join(cur_dir, "visuals")
 
 if not os.path.isdir(visuals_dir):
     os.mkdir(visuals_dir)
 
+df = pd.read_csv(poke_dir)
+for i in df["classfication"]:
+    print(type(i))
+# print(df.head(5))
